7a34b68be72a9e425f3e2c3504065e97500166ca
sridhardscv <sridhardscv@gmail.com>
2023-10-04 19:33:55 +0530

    Simple comand Line tool

--- main.sh
+++ main.sh
@@ -1,1 +1,23 @@
-echo "Hello World"
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <number1> <number2>"
+  exit 1
+
+else    
+    echo "Arg 1 $1"
+    echo "Arg 2 $2"
+    echo "Total length of Args ${#}"
+    echo $(bc <<< "$1 + $2")
+fi
+
+if [[ "$2" =~ [^0-9] ]]; then
+    echo "Variable is a string"
+
+elif [ -v variable ]; then
+    echo "Varibale is a array"
+
+else 
+    echo "Variable is a integer"
+fi
+
