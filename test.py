c6d956ff27da27838642a2a4c509ffa71bc5995e
SRIDHAR DSCV <sridhardscv@gmail.com>
2023-10-05 23:05:58 +0530

    Docker output Parse

---
+++ test.py
@@ -0,0 +1,14 @@
+import subprocess
+
+command = "docker images"
+
+output = subprocess.check_output(command, shell=True, text=True)
+lines = output.strip().split('\n')[1:]
+
+for i in lines:
+    if (str(i.split()[0]) == 'sridhardscv/simple_calculater'):
+        print(i.split()[6])
+    # print(i.split())
+
+
+
